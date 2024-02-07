7c9ea0fe6af24506d9b8364f2faa735663f1b74a
Gnanaprasuna <ittamgnanaprasuna1755@gmail.com>
2024-02-07 15:35:53 +0530

    Add project files.

---
+++ WebApIAssignment/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApIAssignment.Models;
+
+namespace WebApIAssignment.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApIAssignment.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ WebApIAssignment/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApIAssignment.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string MName { get; set; }
+        public string StarCast { get; set; }
+        public string Director { get; set; }
+        public string Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
